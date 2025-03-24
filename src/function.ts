f71d83aaf82ff5c7ba3e6e3afb848cd2f044ff0c
Andrei Luca <1881266+iamandrewluca@users.noreply.github.com>
2025-03-24 10:54:26 +0200

    feat(function): add constant functions

--- src/function.ts
+++ src/function.ts
@@ -3,3 +3,19 @@ export function noop() {}
 export function identity<T>(value: T): T {
 	return value;
 }
+
+export function falseFn() {
+	return false;
+}
+
+export function trueFn() {
+	return true;
+}
+
+export function nullFn() {
+	return null;
+}
+
+export function undefinedFn() {
+	return undefined;
+}
